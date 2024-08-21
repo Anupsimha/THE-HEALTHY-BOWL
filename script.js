487fd2e384161a7b0b0c0fab9783c6281a1f1fdc
anup <anupsimha2@gmail.com>
2024-08-21 14:35:22 +0530

    Added some features

--- script.js
+++ script.js
@@ -131,4 +131,12 @@ window.onbeforeunload = function () {
     window.scrollTo(0, 0);
 };
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+document.querySelectorAll('.accordion-toggle').forEach(button => {
+    button.addEventListener('click', () => {
+      const content = button.nextElementSibling;
+      content.style.display = content.style.display === 'block' ? 'none' : 'block';
+    });
+  });
+  
