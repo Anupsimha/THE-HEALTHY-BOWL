c7a0e1c02bd38c0d0d5c8fc0ddd4b12e35101827
nirbheysingh <nirbhey07@gmail.com>
2024-09-02 03:13:55 +0530

    modifying login page

--- login.js
+++ login.js
@@ -1,10 +1,35 @@
-document.getElementById('captcha-check').addEventListener('change', function() {
-    const submitButton = document.querySelector('.submit-btn');
-    if (this.checked) {
-        submitButton.disabled = false;
-        submitButton.classList.add('active');
-    } else {
-        submitButton.disabled = true;
-        submitButton.classList.remove('active');
-    }
-});
+let sign = document.getElementsByClassName("login")[0];
+let right = document.getElementsByClassName("right")[0];
+let container = document.getElementsByClassName("container")[0];
+let image = document.getElementsByTagName("img")[0];
+
+// sign.addEventListener("click", ()=> {
+//    right.innerHTML = '<div class="container"><img class="logo" src="./images/website_logo.png" alt="logo" height="50px" width="50px"><h1>Healthy Bowl.com</h1><h2>Create an Account!</h2><!-- <button class="google-button">Sign up with Google</button> --> <a class="google-btn" href="https://accounts.google.com/gsi/select?client_id=92879692782-7nd751eusg5fep81a1vhm0r6hf0pdn54.apps.googleusercontent.com&ux_mode=redirect&login_uri=https%3A%2F%2Fwww.codewithharry.com%2Fapi%2Fauth%2Fgooglelogin&ui_mode=card&as=bARcEli5JMufV%2FNTO56LRg&g_csrf_token=be230c1717b6ea4b&origin=https%3A%2F%2Fwww.codewithharry.com"><button class="google-btn"><img src="./images/google btn.png" alt="Google Logo" height="40px" width="200px"></button></a>        <fieldset><legend>OR SIGN UP WITH EMAIL</legend></fieldset><div class="input"> <label for="email">Email Address</label><input type="email" class="email"><label for="email">Password</label><input type="password" class="email"></div><div class="captcha-box"> <input type="checkbox" id="captcha-check"><label for="captcha-check">I am not a robot</label> <img src="./images/recaptcha.png" alt="reCAPTCHA logo" class="captcha-logo"> </div> <button class="create-button">Create an account</button> <fieldset><legend><a href="#" class="signup">OR SIGNUP</a></legend></fieldset> </div>'
+//    let container = document.getElementsByClassName("container")[0];
+//    container.style.height = '610px';
+//    let image = document.getElementsByTagName("img")[0];
+//    console.log(image);
+//    image.style.height = '620px';
+// });
+
+function loadSignupForm(){
+    right.innerHTML = ' <div class="container"> <img class="logo" src="./images/website_logo.png" alt="logo" height="50px" width="50px"><h1>Healthy Bowl.com</h1><h2>Create an Account!</h2> <!-- <button class="google-button">Sign up with Google</button> --><a class="google-btn" href="https://accounts.google.com/gsi/select?client_id=92879692782-7nd751eusg5fep81a1vhm0r6hf0pdn54.apps.googleusercontent.com&ux_mode=redirect&login_uri=https%3A%2F%2Fwww.codewithharry.com%2Fapi%2Fauth%2Fgooglelogin&ui_mode=card&as=bARcEli5JMufV%2FNTO56LRg&g_csrf_token=be230c1717b6ea4b&origin=https%3A%2F%2Fwww.codewithharry.com"><button class="google-btn"><img src="./images/google btn.png" alt="Google Logo" height="40px" width="200px"></button></a>        <fieldset><legend>OR SIGN UP WITH EMAIL</legend></fieldset> <div class="input"> <label for="email">Email Address</label> <input type="email" class="email"></div>  <div class="captcha-box"> <input type="checkbox" id="captcha-check"> <label for="captcha-check">I am not a robot</label> <img src="./images/recaptcha.png" alt="reCAPTCHA logo" class="captcha-logo"></div><button class="create-button">Create an account</button><fieldset><legend><a href="#" class="login">OR LOGIN</a></legend></fieldset> </div>';
+    container.style.height = '530px';
+    image.style.height = '540px';
+
+    let login = document.getElementsByClassName("login")[0];
+    login.addEventListener("click", loadLoginForm);
+}
+
+function loadLoginForm(){
+    right.innerHTML = '<div class="container"><img class="logo" src="./images/website_logo.png" alt="logo" height="50px" width="50px"><h1>Healthy Bowl.com</h1><h2>Create an Account!</h2><!-- <button class="google-button">Sign up with Google</button> --> <a class="google-btn" href="https://accounts.google.com/gsi/select?client_id=92879692782-7nd751eusg5fep81a1vhm0r6hf0pdn54.apps.googleusercontent.com&ux_mode=redirect&login_uri=https%3A%2F%2Fwww.codewithharry.com%2Fapi%2Fauth%2Fgooglelogin&ui_mode=card&as=bARcEli5JMufV%2FNTO56LRg&g_csrf_token=be230c1717b6ea4b&origin=https%3A%2F%2Fwww.codewithharry.com"><button class="google-btn"><img src="./images/google btn.png" alt="Google Logo" height="40px" width="200px"></button></a>        <fieldset><legend>OR SIGN UP WITH EMAIL</legend></fieldset><div class="input"> <label for="email">Email Address</label><input type="email" class="email"><label for="email">Password</label><input type="password" class="email"></div><div class="captcha-box"> <input type="checkbox" id="captcha-check"><label for="captcha-check">I am not a robot</label> <img src="./images/recaptcha.png" alt="reCAPTCHA logo" class="captcha-logo"> </div> <button class="create-button">Create an account</button> <fieldset><legend><a href="#" class="signup">OR SIGNUP</a></legend></fieldset> </div>';
+    container.style.height = '610px';
+    image.style.height = '620px';
+     
+    let signup = document.getElementsByClassName("signup")[0];
+    signup.addEventListener("click", loadSignupForm);
+}
+
+loadSignupForm();
+
+
